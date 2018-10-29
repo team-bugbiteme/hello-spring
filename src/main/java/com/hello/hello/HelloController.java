f14e094652d160dcd25f01a5dccdc9d3291a8e51
Leon Levy <leon.s.levy@gmail.com>
2018-10-29 11:22:08 -0700

    Update HelloController.java

--- src/main/java/com/hello/hello/HelloController.java
+++ src/main/java/com/hello/hello/HelloController.java
@@ -21,7 +21,7 @@ public class HelloController {
     @RequestMapping("/api")
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public Map<String, String> api() {
-        HashMap<String, String> map= new HashMap<>();
+        HashMap<String, String> map = new HashMap<>();
         map.put("greeting", greeting);
         return map;
     }
